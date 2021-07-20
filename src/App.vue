711f63a8456da95dcf2e1a502aa864e9d8c3baea
zhiyou-1070 <2996514896@qq.com>
2021-07-20 16:05:21 +0800

    提交

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="app">
+    <div id="nav">
+      <h1>hello</h1>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "App"
+  
+}
+</script>
+
+<style>
+</style>
